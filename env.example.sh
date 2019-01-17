60af494c116ebdde12962570dce53acee2df5347
Duyet Le <lvduit08@gmail.com>
2019-01-17 09:58:09 +0700

    Update .gitignore

---
+++ env.example.sh
@@ -0,0 +1,5 @@
+firebase functions:config:set pricetrack.sendgrid_key=
+firebase functions:config:set pricetrack.sentry_dsn=
+firebase functions:config:set pricetrack.cronjob_key=
+firebase functions:config:set pricetrack.apiKey=
+firebase functions:config:set pricetrack.admin_token=
