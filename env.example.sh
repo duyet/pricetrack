8c42c7ee627204142c5ae7aa2b76b2e81f1825e9
Duyet Le <lvduit08@gmail.com>
2019-01-21 13:24:45 +0700

    Add env for email

--- env.example.sh
+++ env.example.sh
@@ -3,3 +3,5 @@ firebase functions:config:set pricetrack.sentry_dsn=
 firebase functions:config:set pricetrack.cronjob_key=
 firebase functions:config:set pricetrack.apiKey=
 firebase functions:config:set pricetrack.admin_token=
+firebase functions:config:set pricetrack.gmail_email=
+firebase functions:config:set pricetrack.gmail_password=
