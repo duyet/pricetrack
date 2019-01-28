4d111b54f6535dcae4d83425746eeac7bf0a561e
Duyet Le <lvduit08@gmail.com>
2019-01-29 00:38:09 +0700

    Fix utils

--- functions/utils/config.js
+++ functions/utils/config.js
@@ -1,5 +1,32 @@
 const functions = require('firebase-functions')
 
+/**
+ * Get config from firebase config
+ * config().pricetrack.<KEY>
+ * 
+ * @param key {string} Key to get
+ * @param default_val {object}
+ * @return {object}
+ */
+const getConfig = (key, default_val=false) => {
+  const configSet = functions.config().pricetrack || {}
+  return configSet[key] || default_val
+}
+
+/**
+ * Get sort key from req.params
+ * @param  {[type]} key [description]
+ * @return {[type]}     [description]
+ */
+const getSortKey = key => {
+  const defaultKey = 'created_at'
+  const validKeys = ['created_at', 'last_pull_at', 'created_at', 
+                     'last_update_at', 'price_change', 'price_change_at']
+  if (!key || validKeys.indexOf(key) == -1) return defaultKey
+  return key
+}
+
+
 const IS_PROD = process.env.GCP_PROJECT && process.env.FUNCTION_REGION ? true : false
 console.log(`IS_PROD: ${IS_PROD} `
             + `GCP_PROJECT="${process.env.GCP_PROJECT}" `
@@ -24,33 +51,6 @@ const functionsUrlAsia = !IS_PROD
 const hostingUrl = !IS_PROD
   ? `http://localhost:8000`
   : getConfig('hosting_url', 'https://tracker.duyet.net')
-
-
-/**
- * Get config from firebase config
- * config().pricetrack.<KEY>
- * 
- * @param key {string} Key to get
- * @param default_val {object}
- * @return {object}
- */
-const getConfig = (key, default_val=false) => {
-  const config_set = functions.config().pricetrack || {}
-  return config_set[key] || default_val
-}
-
-/**
- * Get sort key from req.params
- * @param  {[type]} key [description]
- * @return {[type]}     [description]
- */
-const getSortKey = key => {
-  const default_key = 'created_at'
-  const validKeys = ['created_at', 'last_pull_at', 'created_at', 
-                     'last_update_at', 'price_change', 'price_change_at']
-  if (!key || validKeys.indexOf(key) == -1) return default_key
-  return key
-}
 
 
 
