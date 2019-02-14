6b439ae3f420e47e2b4c9f5b6103d40528ae0fcf
Duyet Le <lvduit08@gmail.com>
2019-02-15 01:04:32 +0700

    Block LOGO

---
+++ hosting/src/components/Block/Logo.js
@@ -0,0 +1,6 @@
+import React from "react"
+
+import { LOGO } from '../../constants/config'
+import { HOME } from '../../constants/routes'
+
+export default () => <a className="text-muted" href={HOME}>{LOGO}</a>
