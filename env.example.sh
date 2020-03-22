db4148742ef07fd864aafd674ffdee3dafdcc790
Duyet Le <lvduit08@gmail.com>
2020-03-22 15:24:30 +0700

    update env.example.sh

--- env.example.sh
+++ env.example.sh
@@ -9,4 +9,8 @@ firebase functions:config:set pricetrack.accesstrade_deeplink_base=
 firebase functions:config:set pricetrack.accesstrade_token=
 firebase functions:config:set pricetrack.admin_email=
 firebase functions:config:set pricetrack.cashback_rate=0.5
+
+# in case of deploy /pullData function to worker VM, use this config to make
+# /cronjob trigger to this url
+# e.g. http://worker.pricetrack.duyet.net/duyet-price-tracker/us-central1
 firebase functions:config:set pricetrack.worker_custom_domain=
